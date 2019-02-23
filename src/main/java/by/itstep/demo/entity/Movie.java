bfc265c9c33711c49be2e51bed5f9d7a0413cd9c
группы полустационара <pst@itstep.lan>
2019-02-23 12:48:17 +0300

    first commit here

---
+++ src/main/java/by/itstep/demo/entity/Movie.java
@@ -0,0 +1,18 @@
+package by.itstep.demo.entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+
+    String title;
+    String date;
+    String author;
+    String genre;
+
+}
---
+++ src/main/webaps/index.jsp
@@ -0,0 +1,11 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<h1>Demo</h1>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
